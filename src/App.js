c1b4cd49f81bc48d2c2d62d8a1302f4118cd2671
shingfai <dicky@itconsult.hk>
2023-08-17 22:44:22 +0800

    nnn

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
  <div className="App">
  <header className="App-header">
  <img src={logo} className="App-logo" alt="logo" />
- <p>Hello my friend. </p>
+ <p>Hello my friend0000000-----. </p>
  
  </header>
  </div>
