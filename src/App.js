90b2cf8e568d4da98356a7e589d97bddc25e8348
shingfai <dicky@itconsult.hk>
2023-08-17 10:58:15 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ return (
+ <div className="App">
+ <header className="App-header">
+ <img src={logo} className="App-logo" alt="logo" />
+ <p>
+ ///////////////Hello World! My Dear Friend
+ <p>---------高效節能！一體化機櫃最佳之選......</p>
+ </p>
+ </header>
+ </div>
+ );
+}
+
+export default App;
