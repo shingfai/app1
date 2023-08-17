c238f95d4c0c621ad29abe8648d4c4c5cd5cfec3
shingfai <dicky@itconsult.hk>
2023-08-17 22:58:47 +0800

    1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
  <div className="App">
  <header className="App-header">
  <img src={logo} className="App-logo" alt="logo" />
- <p>盛輝科技為企業提供優質的服務</p>
+ <p>----盛輝科技為企業提供優質的服務----</p>
 <p>---------最佳之選--------</p>
  
  </header>
