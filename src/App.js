fb19afceff5fc9e3b0883dc58ee4c66150a19901
shingfai <dicky@itconsult.hk>
2023-08-17 22:31:33 +0800

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
  <header className="App-header">
  <img src={logo} className="App-logo" alt="logo" />
  <p>
- 盛輝科技優質的服務
+ ---------盛輝科技優質的服務---------
  <p>---------最佳之選--------</p>
  </p>
  </header>
