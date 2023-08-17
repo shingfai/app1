2e1c6a6992bf3aec91887db5e4ed6bd1d10bea90
shingfai <dicky@itconsult.hk>
2023-08-17 12:07:56 +0800

    1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
  <header className="App-header">
  <img src={logo} className="App-logo" alt="logo" />
  <p>
- ///////////////Hello World! My Dear Friend
+ *******Hello World! My Dear Friend
  <p>---------高效節能！一體化機櫃最佳之選......</p>
  </p>
  </header>
